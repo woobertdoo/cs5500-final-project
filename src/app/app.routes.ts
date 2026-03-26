4e6479cbe89741aaf83713b29046b460c5e415bb
Eldon Hopp <elhf8@umsystem.edu>
2026-03-26 16:23:08 -0500

    Built hero section with Tailwind and added a disclsure that we are working off a real website

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Hero } from './home/hero/hero';
+import { Form } from './contact/form/form';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Hero },
+  { path: 'contact', component: Form }
+];
--- src/app/home/hero/hero.ts
+++ src/app/home/hero/hero.ts
@@ -2,10 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
-  imports: [],
+  standalone: true,
   templateUrl: './hero.html',
-  styleUrl: './hero.css',
+  styleUrl: './hero.css'
 })
-export class Hero {
-
-}
+export class Hero {}
