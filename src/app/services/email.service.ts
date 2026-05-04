8b22f6036c9798a142160f7ed358fb9f3ed05956
woobertdoo <jamie.a.harris97@gmail.com>
2026-05-04 15:07:06 -0500

    update angular environments

--- src/app/services/email.service.ts
+++ src/app/services/email.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 export interface ContactRequest {
   name: string;
@@ -19,6 +20,6 @@ export class EmailService {
   private http = inject(HttpClient);
 
   sendContactEmail(data: ContactRequest): Observable<ApiResponse> {
-    return this.http.post<ApiResponse>("http://localhost:3000/api/contact", data);
+    return this.http.post<ApiResponse>(`${environment.apiUrl}/contact`, data);
   }
 }
