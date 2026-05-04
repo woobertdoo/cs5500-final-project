fd0194fe12223ea7b5a5860c636baca0a8ca1b58
woobertdoo <jamie.a.harris97@gmail.com>
2026-05-04 15:01:31 -0500

    Updating server and vercel for CORS

--- server/server.js
+++ server/server.js
@@ -9,6 +9,13 @@ const app = express();
 const port = 3000;
 const isProd = process.env.NODE_ENV === 'production';
 
+app.options('/{*path}', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.sendStatus(204);
+});
+
 app.use(cors());
 
 app.use(express.json());
