cde53e78d076a15c0584b609d5933ade040a51dd
woobertdoo <jamie.a.harris97@gmail.com>
2026-05-04 14:51:10 -0500

    update CORS policy

--- server/server.js
+++ server/server.js
@@ -9,9 +9,7 @@ const app = express();
 const port = 3000;
 const isProd = process.env.NODE_ENV === 'production';
 
-if (!isProd) {
-  app.use(cors({ origin: 'http://localhost:4200' }));
-}
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
